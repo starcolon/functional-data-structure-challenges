d3ddd7ece29cb1577776cea23999719cd8f4582f
starcolon <tao@starcolon.com>
2018-11-08 23:02:54 +0100

    graph initial structure

--- src/Data/Connection/Base/Graph.hs
+++ src/Data/Connection/Base/Graph.hs
@@ -2,10 +2,11 @@ module Data.Connection.Base.Graph where
 
 import qualified Data.Map as Map
 
-data Ord a => Node a = NNode | Node a
+data Ord a => Node a = NNode | Node { size :: a, id :: Int,  out :: Map.Map Int (Link a) }
 
 data Ord a => Link a = NLink | Link { mag :: a, from :: Int, to :: Int }
 
+-- Graph is a [Map] of identification number -> to [Node]
 data Ord a => Gr a = Gr (Map.Map Int (Node a))
 
 -- Graph size by number of nodes
