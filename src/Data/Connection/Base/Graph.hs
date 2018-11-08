b517954acdb90429b95d8e42d1f9b3977a54cf9d
starcolon <tao@starcolon.com>
2018-11-08 23:09:11 +0100

    wip: draft of graph functions

--- src/Data/Connection/Base/Graph.hs
+++ src/Data/Connection/Base/Graph.hs
@@ -6,6 +6,8 @@ data Ord a => Node a = NNode | Node { size :: a, id :: Int,  out :: Map.Map Int 
 
 data Ord a => Link a = NLink | Link { mag :: a, from :: Int, to :: Int }
 
+data Route = Route Int Int
+
 -- Graph is a [Map] of identification number -> to [Node]
 data Ord a => Gr a = Gr (Map.Map Int (Node a))
 
@@ -13,3 +15,14 @@ data Ord a => Gr a = Gr (Map.Map Int (Node a))
 gsize :: Ord a => Gr a -> Int
 gsize g = error "TAOTODO:"
 
+-- Query links between two nodes
+between :: Ord a => Gr a -> Route -> Maybe (Link a)
+between g (Route i j) = error "TAOTODO:"
+
+-- Find all possible routes from a node to another
+traverse :: Ord a => Gr a -> Route -> [[Link a]]
+traverse g (Route i j) = error "TAOTODO:"
+
+-- Find the shortest path between two nodes
+shortest :: Ord a => Gr a -> Route -> [Link a]
+shortest g (Route i j) = error "TAOTODO:"
