0e4e3b52e60087f2fac0e7e91c5a4c538e84af14
starcolon <tao@starcolon.com>
2018-11-18 21:49:37 +0100

    wip: add initial graph spec

--- src/Data/Connection/Base/Graph.hs
+++ src/Data/Connection/Base/Graph.hs
@@ -2,35 +2,9 @@ module Data.Connection.Base.Graph where
 
 import qualified Data.Map as Map
 
-data Ord a => Node a = NNode | Node { size :: a, id :: Int,  out :: Map.Map Int (Link a) }
-
-data Ord a => Link a = NLink | Link { mag :: a, from :: Int, to :: Int }
-
+data Eq a => Node a = Node { size :: Double, id :: a, out :: Map.Map a (Link a) }
+data Eq a => Link a = Link { mag :: Double, from :: a, to :: a }
 data Route = Route Int Int
 
 -- Graph is a [Map] of identification number -> to [Node]
-data Ord a => Gr a = Gr (Map.Map Int (Node a))
-
--- Graph size by number of nodes
-gsize :: Ord a => Gr a -> Int
-gsize g = error "TAOTODO:"
-
--- Add a new node
-(++) :: Ord a => Gr a -> Node a -> Gr a 
-(++) g n = error "TAOTODO:"
-
--- Add a new link 
-(**) :: Ord a => Gr a -> Link a -> Gr a
-(**) g l = error "TAOTODO:"
-
--- Query links between two nodes
-paths :: Ord a => Gr a -> Route -> Maybe (Link a)
-paths g (Route i j) = error "TAOTODO:"
-
--- Find all possible routes from a node to another
-traverse :: Ord a => Gr a -> Route -> [[Link a]]
-traverse g (Route i j) = error "TAOTODO:"
-
--- Find the shortest path between two nodes
-shortest :: Ord a => Gr a -> Route -> [Link a]
-shortest g (Route i j) = error "TAOTODO:"
+data Eq a => Gr a = Gr (Map.Map Int (Node a))
---
+++ test/Data/Connection/Action/GraphSpec.hs
@@ -0,0 +1,27 @@
+module Data.Connection.Action.GraphSpec (main,spec) where
+
+import Test.Hspec
+import Test.QuickCheck()
+import Data.Maybe()
+import Data.Connection.Base.Graph(Gr,Route,Node,Link)
+import Prelude hiding (min, max)
+import Data.Maybe(fromJust)
+import qualified Data.Map as Map
+--import Data.Connection.Action.Graph()
+
+main :: IO ()
+main = hspec spec
+
+g5 :: Gr Double 
+g5 = Gr (Map.Map )
+
+spec :: Spec
+spec = do
+  describe "Basic Graph functions" $ do
+
+    it "get size of a graph" $ do 
+      error "TAOTODO:"
+
+    it "get all paths from A to B" $ do 
+      error "TAOTODO:"
+
