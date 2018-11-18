02e73aa1f292e78783795ab8d2025cc6ff595ffb
starcolon <tao@starcolon.com>
2018-11-18 22:45:32 +0100

    wip: prototype

--- src/Data/Connection/Base/Graph.hs
+++ src/Data/Connection/Base/Graph.hs
@@ -1,10 +1,17 @@
 module Data.Connection.Base.Graph where
 
-import qualified Data.Map as Map
+import qualified Data.Map as M
+import qualified Data.Set as S
 
-data Eq a => Node a = Node { size :: Double, id :: a, out :: Map.Map a (Link a) }
+data Eq a => Node a = Node { value :: Double, id :: a, out :: M.Map a (Link a) }
 data Eq a => Link a = Link { mag :: Double, from :: a, to :: a }
 data Route = Route Int Int
 
 -- Graph is a [Map] of identification number -> to [Node]
-data Eq a => Gr a = Gr (Map.Map Int (Node a))
+data Eq a => Gr a = Gr (S.Set (Node a))
+
+size :: Eq a => Gr a -> Int 
+size (Gr g) = S.size $ g
+
+new :: Eq a => S.Set (Node a) -> Gr a 
+new s = error "TAOTODO:"
