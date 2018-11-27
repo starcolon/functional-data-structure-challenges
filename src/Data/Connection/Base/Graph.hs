25fc42ee9ebf1f3cda42a1b2f2feac220482ca52
starcolon <tao@starcolon.com>
2018-11-27 23:38:22 +0100

    wip: ord

--- src/Data/Connection/Base/Graph.hs
+++ src/Data/Connection/Base/Graph.hs
@@ -3,31 +3,32 @@ module Data.Connection.Base.Graph where
 import qualified Data.Map as M
 
 -- Graph of vertex type [v]
-data G v = G ([V v]) (M.Map v (E v)) -- TAOTODO: Should implement a better tree-based list of [V v]
+data Ord v => G v = G ([V v]) (M.Map v (E v)) -- TAOTODO: Should implement a better tree-based list of [V v]
 -- Vertex of type [v]
-data V v = NV | V v Double
+data Ord v => V v = NV | V v Double
 -- Edge of type [v]
-data E v = NE | E v v Double
+data Ord v => E v = NE | E v v Double
 
-size :: G v -> Int
+size :: Ord v => G v -> Int
 size (G ns _) = length $ ns
 
 -- Associativity of vertex
 -- f (V v) = V (f v)
-fv :: (v -> v') -> V v -> V v'
+fv :: Ord v => (v -> v') -> V v -> V v'
 fv f v = case v of 
   NV  -> NV
   V a d -> error "TAOTODO:"
 
 -- Associativity of edge
 -- f (E v) = E (f e)
-fe :: (v -> v') -> E v -> E v'
+fe :: Ord v => (v -> v') -> E v -> E v'
 fe f e = case e of 
   NE    -> NE
   E a b d -> error "TAOTODO:"
 
-mapG :: (v -> v') -> G v -> G v'
+mapG :: (Ord v, Ord v') => (v -> v') -> G v -> G v'
 mapG f (G vs m) = G [fv f n | n <- vs] (M.fromList $ [(f a, fe f b) | (a,b) <- M.toList m])
 
-pureG :: v -> G v
+-- Create a unit graph with an initial vertex
+pureG :: Ord v => v -> G v
 pureG v = error "TAOTODO:"
