29207f6ea5665f19a71a67add7338f0a9cebab23
starcolon <tao@starcolon.com>
2018-12-10 21:56:25 +0100

    initial test of graph

--- test/Data/Connection/Action/GraphSpec.hs
+++ test/Data/Connection/Action/GraphSpec.hs
@@ -3,7 +3,11 @@ module Data.Connection.Action.GraphSpec (main,spec) where
 import Test.Hspec
 import Test.QuickCheck()
 import Data.Maybe()
-import Data.Connection.Base.Graph(Gr,Route,Node,Link)
+import Data.Connection.Base.Graph(
+  G,G(G),
+  V,V(V),V(NV),
+  E,E(E),E(NE),
+  size)
 import Prelude hiding (min, max)
 import Data.Maybe(fromJust)
 import qualified Data.Map as Map
@@ -12,22 +16,22 @@ import qualified Data.Map as Map
 main :: IO ()
 main = hspec spec
 
-g3 :: Gr String 
-g3 = Gr [(V "a" 1) (V "b" 1) (V "c" 15)] NE
+g3 :: G String 
+g3 = G [(V "a" 1), (V "b" 1), (V "c" 15)] Map.empty
 
 spec :: Spec
 spec = do
   describe "Basic Graph functions" $ do
 
     it "get size of a graph" $ do 
-      error "TAOTODO:"
+      (size g3) `shouldBe` 3
 
-    it "check the existence of vertex" $ do
-      error "TAOTODO:"
+    -- it "check the existence of vertex" $ do
+    --   error "TAOTODO:"
 
-    it "merge two graphs" $ do 
-      error "TAOTODO:"
+    -- it "merge two graphs" $ do 
+    --   error "TAOTODO:"
 
-    it "get all paths from A to B" $ do 
-      error "TAOTODO:"
+    -- it "get all paths from A to B" $ do 
+    --   error "TAOTODO:"
 
