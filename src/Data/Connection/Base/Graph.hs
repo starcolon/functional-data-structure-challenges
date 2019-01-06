22018c6dc585b7d98532851f98c96b4cec8b8374
starcolon <tao@starcolon.com>
2019-01-06 18:40:04 +0100

    wip: restructure graph

--- src/Data/Connection/Base/Graph.hs
+++ src/Data/Connection/Base/Graph.hs
@@ -1,27 +1,34 @@
 module Data.Connection.Base.Graph where
 
 import qualified Data.Set as S
-import qualified Data.Map as M
+import qualified Data.Map hiding(foldr) as M
 
--- Graph of type [v] implements Vertices of type [v]
-
-data G v = G (M.Map v (V v)) deriving (Show,Eq)
-
--- Vertex consists of a value [v], weight, and a Map of edges
-data V v = NV | V v Double (M.Map v Double)  deriving (Show,Eq)
+-- Graph is a composition of nodes or graphs itself
+newtype E v = M.Map v Double
+data G v = NullG | 
+          V v Double (E v) | 
+          G (M.Map v (G v)) deriving (Show,Eq)
 
 size :: Ord v => G v -> Int
-size (G n) = M.size n
+size (G n) = case n of
+  NullG   -> 0
+  V _ _ _ -> 1
+  G m     -> foldr (+) 0 [size b | (a,b) <- M.toList m]
 
 -- Associativity of vertex
 -- f (V v) = V (f v)
-fv :: (v -> v') -> V v -> V v'
-fv f v = case v of 
-  NV  -> NV
-  V a d m -> V a' d m'
-    where 
-      a' = f a
-      m' = M.fromDistinctAscList $ [(f k,v) | (k,v) <- M.toList m]
+-- fv :: (v -> v') -> V v -> V v'
+-- fv f v = case v of 
+--   NV  -> NV
+--   V a d m -> V a' d m'
+--     where 
+--       a' = f a
+--       m' = M.fromDistinctAscList $ [(f k,v) | (k,v) <- M.toList m]
+
+-- Fold multiple graphs into one
+foldG :: Ord v => [G v] -> G v
+foldG n:[] = n
+foldG a:b:bs = foldG $ (a <+> b):bs
 
 -- Join two graphs
 (<+>) :: Ord v => G v -> G v -> G v
@@ -59,23 +66,22 @@ ensureVertex (V a d m) (G n) = case M.lookup a n of
   Just _ -> G n
 
 mapG :: (v -> v') -> G v -> G v'
-mapG f (G n) = 
-  let n' = M.fromDistinctAscList $ [(f a, v <**> f) | (a,v) <- M.toList n]
-  in G n'
+mapG f (G n) = G M.fromDistinctAscList $ [(f a, v <**> f) | (a,v) <- M.toList n]
 
 -- Create a unit graph with an initial vertex
 pureG :: v -> G v
-pureG v = G (M.singleton v (pureV v))    
+pureG v = G (M.singleton v (pureV v))
 
 -- Create a unit vertex
 pureV :: v -> V v
 pureV v = V v 1 M.empty
 
+explodeG :: G v -> [G v]
+explodeG (G m) = [G (M.singleton k v) | (k,v) <- M.toList m]
+
 flatMapG :: G v -> (v -> G v') -> G v'
-flatMapG _ _ = error "TAOTODO:"
--- flatMapG (G n m) f = G nf' mf' 
---   where n'  = [ | n <- M.toList n ]
---         nf' = ???
---         mf' = ???
+flatMapG (G m) f = G m'
+  where
+    gs = [f g | g <- explodeG (G m)] -- TAOTODO: Explode and union them back together
 
 
