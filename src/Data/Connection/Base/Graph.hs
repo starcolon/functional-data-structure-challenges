966116a0f44fe85f148d668da7e284654d59b973
starcolon <tao@starcolon.com>
2019-01-10 21:53:17 +0100

    wip: rewrite functions

--- src/Data/Connection/Base/Graph.hs
+++ src/Data/Connection/Base/Graph.hs
@@ -4,16 +4,16 @@ import qualified Data.Set as S
 import qualified Data.Map as M
 
 -- Graph is a composition of nodes or graphs itself
-data E v = M.Map v Double deriving (Show,Eq)
+type E v = M.Map v Double
 data G v = NullG | 
           V v Double (E v) | 
           G (M.Map v (G v)) deriving (Show,Eq)
 
 size :: Ord v => G v -> Int
-size (G n) = case n of
+size g = case g of
   NullG   -> 0
   V _ _ _ -> 1
-  G m     -> Prelude.foldr $ (+) 0 [size b | (a,b) <- M.toList m]
+  G m     -> Prelude.foldr (+) 0 [size b | (a,b) <- M.toList m]
 
 coreMaybe :: Ord v => G v -> Maybe (v, Double, E v)
 coreMaybe NullG = Nothing
@@ -28,20 +28,21 @@ coreMaybe (G m) =
 iter :: G v -> [(v, Double, E v)]
 iter NullG = []
 iter (V v d e) = [(v, d, e)]
-iter (G m) = Prelude.foldr $ (++) [] [iter g | (v,g) <- M.toList m]
+iter (G m) = Prelude.foldr (++) [] [iter g | (v,g) <- M.toList m]
 
 -- Fold multiple graphs into one
 foldG :: Ord v => [G v] -> G v
 foldG [] = error "Unable to fold empty list of graphs"
 foldG (n:[]) = n
-foldG (a:b:bs) = foldG $ (a <+> b):bs
+foldG (a:b:bs) = foldG (h':bs)
+  where h' = (a <+> b)
 
 -- Join two graphs
 (<+>) :: Ord v => G v -> G v -> G v
 (<+>) NullG n = n
 (<+>) n NullG = n
 (<+>) (V v1 d1 e1) (V v2 d2 e2) = G (M.fromDistinctAscList [(v1, V v1 d1 e1), (v2, V v2 d2 e2)])
-(<+>) (V v d e) (G m) = G (M.insert $ v (V v d e) m)
+(<+>) (V v d e) (G m) = G (M.insert v (V v d e) m)
 (<+>) (G m) (V v d e) = (V v d e) <+> (G m)
 (<+>) (G m1) (G m2) = G (M.union m1 m2)
 
@@ -54,9 +55,9 @@ has (G n) v = any pred gs
 applyE :: (v -> v') -> E v -> E v'
 applyE f m = M.fromDistinctAscList [(f v,d) | (v,d) <- M.toList m]
 
-mapG :: (v -> v') -> G v -> G v'
+mapG :: (Ord v, Ord v') => (v -> v') -> G v -> G v'
 mapG f NullG = NullG
-mapG f (V v d e) = V (f v) d e
+mapG f (V v d e) = V (f v) d (applyE f e)
 mapG f (G m) = foldG gs
   where
     gs = [V (f v) d (applyE f e) | (v,d,e) <- iter $ G m]
@@ -69,5 +70,5 @@ unitG :: v -> G v
 unitG v = V v 1 M.empty
 
 flatMapG :: G v -> (v -> G v') -> G v'
-flatMapG g f = foldG [(f v,d,applyE f e) | (v,d,e) <- iter $ g]
+flatMapG g f = foldG [V (f v) d (applyE f e) | (v,d,e) <- iter g]
 
